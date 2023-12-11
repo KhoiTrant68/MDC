9aa74a6abe14fe58e3616d504f3618f10d377d15
KhoiTrant <85048572+KhoiTrant68@users.noreply.github.com>
2023-12-11 14:50:46 +0700

    model

--- train.sh
+++ train.sh
@@ -1,7 +1,6 @@
 python main_compress.py \
 -d ./datasets/imagenet \
 --checkpoint ./checkpoint/mae_pretrain_vit_base.pth \
---input_size 224 \
 --epochs 1000 \
 --batch_size 32 \
 --output_dir ./output_dir \
